ae87b9fe7072f955979278bbcbb5f5ada9165dbf
Heather Etheridge <heathernetheridge@gmail.com>
2019-09-10 14:22:47 -0500

    Adds about and home routes and templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+# The home view
+def home(request):
+    return HttpResponse('Hello There')
+
+def about(request):
+    return render(request, 'about.html')
