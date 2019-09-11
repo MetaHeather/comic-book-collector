6ae13ab15f2938134429caef81a0966075c251dc
Heather Etheridge <heathernetheridge@gmail.com>
2019-09-11 08:48:23 -0500

    Adds super user and registers Comic model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Comic(models.Model):
+    title = models.CharField(max_length=100)
+    issue = models.IntegerField()
+    author = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    publishdate = models.DateField()
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,6 @@
 from django.shortcuts import render
+from .models import Comic
 # Create your views here.
-
-class Comic:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, title, issue, author, description, publishdate):
-    self.title = title
-    self.issue = issue
-    self.author = author
-    self.description = description
-    self.publishdate = publishdate
-
-comics = [
-  Comic('Saga', '1', 'Brian K. Vaughan', 'Space war opera', 'Mar 2012'),
-  Comic('Preacher', '5', 'Garth Ennis', 'Post appocalyptic situatons', 'Aug 1995'),
-  Comic('The Walking Dead', '1', 'Robert Kirkman', 'Zombie appocalypse', 'Oct 2013')
-]
 
 # The home view
 def home(request):
@@ -23,7 +10,10 @@ def about(request):
     return render(request, 'about.html')
 # comics index
 def comics_index(request):
-  return render(request, 'comics/index.html', { 'comics': comics })
+    comics = Comics.objects.all()
+    return render(request, 'comics/index.html', { 'comics': comics })
+
+    
 
 
 
