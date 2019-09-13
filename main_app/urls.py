b8198cbac7d354e800442f95a47b8186a689bf82
Heather Etheridge <heathernetheridge@gmail.com>
2019-09-13 08:27:41 -0500

    Adds detail route and html

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     # route to view all comics
-    path('comics/', views.comics_index, name='index'),    
+    path('comics/', views.comics_index, name='index'),  
+    # route to view single comic's details
+    path('comics/<int:comic_id>/', views.comics_detail, name='detail')  
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -10,10 +10,10 @@ def about(request):
     return render(request, 'about.html')
 # comics index
 def comics_index(request):
-    comics = Comics.objects.all()
+    comics = Comic.objects.all()
     return render(request, 'comics/index.html', { 'comics': comics })
-
-    
-
-
+# comic detail
+def comics_detail(request, comic_id):
+    comic = Comic.objects.get(id=comic_id)
+    return render(request, 'comics/detail.hmtl', {'comic': comic})
 
